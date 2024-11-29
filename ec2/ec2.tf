262885b1d72ef6e0007179f5678fc4abab2610e9
harriet <tenguhh@yahoo.com>
2024-11-29 14:47:36 +0100

    improved code

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "web_server" {
   # best practices as per checkov scanner
   monitoring    = true
   ebs_optimized = true
-   root_block_device {
+  root_block_device {
      encrypted = true
    }
 
