e0f5a7fd24c83a75829278386c1b590ef961cd53
chiella <chiellaeb@gmail.com>
2024-12-10 12:58:13 +0100

    modified code

--- iam/iam.tf
+++ iam/iam.tf
@@ -9,10 +9,10 @@ variable "tags" {
 }
 
 resource "aws_iam_role" "iam_role" {
-  name = "${var.name}-iam-role"
+  name = join("", [var.name, "-", "iam-role"])
 
-   #Terraform's "jsonencode" function converts a
-   #Terraform expression result to valid JSON syntax.
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -26,78 +26,27 @@ resource "aws_iam_role" "iam_role" {
       },
     ]
   })
+
+  inline_policy {
+    name = join("", [var.name, "-", "iam-policy"])
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "logs:CreateLogStream",
+            "logs:DescribeLogStreams",
+            "logs:CreateLogGroup",
+            "logs:PutLogEvents"
+
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
   tags = var.tags
 }
-
-resource "aws_iam_role_policy" "test_policy" {
-  name = "test_policy"
-  role = aws_iam_role.test_policy.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-variable "name" {
-  type        = string
-  description = "name tag value"
-}
-
-variable "tags" {
-  type        = map(any)
-  description = "tags for the vpc module"
-}
-
-#resource "aws_iam_role" "iam_role" {
- # name = join("", [var.name, "-", "iam-role"])
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
- # assume_role_policy = jsonencode({
-  #  Version = "2012-10-17"
-   # Statement = [
-     # {
-       # Action = "sts:AssumeRole"
-       # Effect = "Allow"
-       # Sid    = ""
-        #Principal = {
-         # Service = "ec2.amazonaws.com"
-       # }
-     # },
-    #]
-  #})
-
-  #inline_policy {
-    #name = join("", [var.name, "-", "iam-policy"])
-
-    #policy = jsonencode({
-    #  Version = "2012-10-17"
-     # Statement = [
-        #{
-         # Action = [
-          #  "logs:CreateLogStream",
-           # "logs:DescribeLogStreams",
-           # "logs:CreateLogGroup",
-           # "logs:PutLogEvents"
-
-          #]
-         # Effect   = "Allow"
-         # Resource = "*"
-      #  },
-    #  ]
-   # })
-  #}
-
- # tags = var.tags
-#}
